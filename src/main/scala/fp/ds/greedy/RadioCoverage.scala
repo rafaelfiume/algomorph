06a9ee4c779d81cbbb23071a857b77346d9b5bba
Rafael Fiume <rafael.fiume@yahoo.it>
2020-01-28 09:41:29 +0000

    Greedy algorithm to select (one of) the best combination of stations to cover required states

---
+++ src/main/scala/fp/ds/greedy/RadioCoverage.scala
@@ -0,0 +1,33 @@
+package fp.ds.greedy
+
+import scala.annotation.tailrec
+
+// 1. Check the states to cover
+// 2. Pick up the station that covers more state (local optimal solution)
+// 3. Add it to the list bestStations and remove it from available stations
+// 4. Are there states uncovered? If so, repeat the process
+object RadioCoverage {
+
+  type Radio = String
+  type State = String
+
+  def best(stations: Map[Radio, Set[State]])(statesToCover: Set[State]): Set[Radio] = {
+
+    @tailrec def search(stations: Map[Radio, Set[State]], statesToCover: Set[State], bestStations: Set[Radio]): Set[Radio] = {
+      if (statesToCover.isEmpty) return bestStations
+
+      val availableStations = stations.keys
+      val head = availableStations.head
+      val newBestStation = availableStations.tail.foldLeft(head) { (a, b) =>
+        val remainingAfterA = statesToCover diff stations(a)
+        val remainingAfterB = statesToCover diff stations(b)
+        if (remainingAfterA.size < remainingAfterB.size) a else b
+      }
+      val newlyCoveredStates = stations(newBestStation)
+      search(stations.removed(newBestStation), statesToCover diff newlyCoveredStates, bestStations + newBestStation)
+    }
+
+    search(stations, statesToCover, Set.empty)
+  }
+
+}
---
+++ src/test/scala/fp.ds/greedy/RadioCoverageSpec.scala
@@ -0,0 +1,23 @@
+package fp.ds.greedy
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+class RadioCoverageSpec extends FlatSpec with Matchers {
+
+  "bestCoverage" should "return the minimum number of radio stations that covers all the required states" in {
+    val statesNeeded = Set("mt", "wa", "or", "id", "nv", "ut", "ca", "az")
+    val stations = Map(
+      "kone"   -> Set("id", "nv", "ut"),
+      "ktwo"   -> Set("wa", "id", "mt"),
+      "kthree" -> Set("or", "nv", "ca"),
+      "kfour"  -> Set("nv", "ut"),
+      "kfive"  -> Set("ca", "az")
+    )
+
+    val result = RadioCoverage.best(stations)(statesNeeded)
+
+    result should contain theSameElementsAs Set("kthree", "ktwo", "kfour", "kfive")
+    stations.filter { case (k, _) => result.contains(k) }.values.toSet.flatten shouldBe statesNeeded
+  }
+
+}

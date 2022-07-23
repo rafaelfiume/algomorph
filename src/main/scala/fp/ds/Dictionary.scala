b21dfb9cbd2c584f7274c8cc736f9042abedc928
Rafael Fiume <rafael.fiume@yahoo.it>
2022-07-23 15:36:04 +0100

    Fix compiling error

--- src/main/scala/fp/ds/Dictionary.scala
+++ src/main/scala/fp/ds/Dictionary.scala
@@ -11,6 +11,7 @@ object Dictionary {
     case Branch((k, _), _, _) if k == key => sys.error(s"key <$key> already exists in the dictionary")
     case Branch((k, v), l, r) if k > key => Branch((k, v), insert(l, key, value), r)
     case Branch((k, v), l, r) if k < key => Branch((k, v), l, insert(r, key, value))
+    case e => sys.error(s"Unexpected $e") // just to make compiler happy
   }
 
   def update[A](d: Dictionary[A], key: String, value: A): Dictionary[A] = d match {
@@ -18,6 +19,7 @@ object Dictionary {
     case Branch((k, v), l, r) if k == key => Branch((k, v), l, r)
     case Branch((k, v), l, r) if k > key => Branch((k, v), update(l, key, value), r)
     case Branch((k, v), l, r) if k < key => Branch((k, v), l, update(r, key, value))
+    case e => sys.error(s"Unexpected $e") // just to make compiler happy
   }
 
   def search[A](d: Dictionary[A], key: String): Option[A] = d match {
@@ -25,6 +27,7 @@ object Dictionary {
     case Branch((k, v), _, _) if k == key => Some(v)
     case Branch((k, _), l, _) if k > key => search(l, key)
     case Branch((k, _), _, r) if k < key => search(r, key)
+    case e => sys.error(s"Unexpected $e") // just to make compiler happy
   }
 
 }

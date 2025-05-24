78245ee6ea445bebea36be0d5a9814def1ba64dc
Rafael Fiume <rafael.fiume@yahoo.it>
2025-05-24 21:50:51 +0100

    Fun with prime numbers, lazy evaluation and mutual recursion between functions (isPrime/primes)

---
+++ src/main/scala/numbers/Primes.scala
@@ -0,0 +1,65 @@
+package numbers
+
+import Math.sqrt
+
+object Primes:
+
+  /**
+   * Checks if a number is prime using trial division.
+   */
+  def isPrime(n: Int): Boolean =
+    if n < 2 then false
+    else if n == 2 then true
+    else if n % 2 == 0 then false
+    else primes.takeWhile(_ <= sqrt(n)).forall(n % _ != 0)
+
+  /**
+   * Lazily evaluates prime numbers.
+   *
+   * This implementation is stack-safe but very slow. It should be useful for reference purposes or when dealing with big primes
+   * is not a requirement.
+   *
+   * ===Complexity (to the best of my knowledge)===
+   *   - Space: Θ(1)
+   */
+  def primes: LazyList[Int] = 2 #:: LazyList.from(3, 2).filter(isPrime)
+
+  /**
+   * A basic implementation of Sieve of Eratosthenes.
+   *
+   * ===Algorithm===
+   *
+   * Given an integer > 1 and a set P = {2, 3, 4, ..., n}:
+   *   1. For each p ∈ P where p² <= n:
+   *      1. Remove all multiples of p from P except for p itself.
+   *   1. The remaining elements in P are prime numbers.
+   *
+   * ===Evaluation Semantics===
+   * {{{
+   * sieve(25):
+   *   - Initial: [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+   *   - p=2: [2,3,5,7,9,11,13,15,17,19,21,23,25]
+   *   - p=3: [2,3,5,7,11,13,17,19,21,23,25]
+   *   - p=4: sieve(4) = false -> Skip
+   *   - p=5: [2,3,5,7,11,13,17,19,21,23]
+   *   - p=6: p > sqrt(25) -> StopS
+   * }}}
+   *
+   * ===Complexity===
+   *   - Time: Θ(n log log n) - to the best of my knowledge
+   *   - Space: Θ(n)
+   */
+  def sieve(n: Int): Vector[Int] =
+    require(n > 1)
+
+    val sieve = Array.fill(n + 1)(true)
+    sieve(0) = false
+    sieve(1) = false
+
+    for
+      p <- 2 to sqrt(n).toInt
+      if sieve(p)
+      multiple <- p * p to n by p
+    do sieve(multiple) = false
+
+    (2 to n).filter(sieve).toVector
---
+++ src/test/scala/numbers/PrimesSpec.scala
@@ -0,0 +1,32 @@
+package numbers
+
+import numbers.Primes.*
+import munit.FunSuite
+
+class PrimesSpec extends FunSuite:
+
+  test("checks if prime"):
+    for p <- Set(2, 3, 5, 7, 11, 13, 17, 19) do assert(isPrime(p), s"$p is prime")
+
+  test("checks if composite"):
+    for c <- Set(4, 6, 8, 9, 10, 12, 14, 15, 16, 18) do assert(!isPrime(c), s"$c is not prime")
+
+  test("generates primes"):
+    assertEquals(primes(999), 7919)
+    assertEquals(primes.take(10).toList, expected = List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29))
+
+  // the test succeeds, but it takes 106m to return the number of primes < 10M.
+  test("primes is stack-safe".ignore):
+    // Prime Number Theory: number of primes not exceeding n is roughly n/ln n.
+    // Let n = 10,000,000, then π(n) = 620,420. The actual number is 664,579.
+    assertEquals(primes.takeWhile(_ <= 10_000_000).length, expected = 664_579)
+
+  test("sieve returns primes"):
+    assertEquals(sieve(2), Vector(2))
+    assertEquals(sieve(30), expected = Vector(2, 3, 5, 7, 11, 13, 17, 19, 23, 29))
+    assertEquals(sieve(100).length, expected = 25) // 25 primes < 100
+    assert(sieve(1000).forall { prime => (2 until prime).forall(prime % _ != 0) }) // no composites
+
+  test("sieve is stack-safe"):
+    // completes in around 3s!! `sives` is many order of magnitude faster than `primes` (see above)
+    assertEquals(sieve(10_000_000).length, expected = 664_579)
